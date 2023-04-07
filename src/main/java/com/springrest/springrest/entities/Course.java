84515a96dac42b9992670a0b518991b488953a75
riteshsinha <ritesh.sinha@nagarro.com>
2023-04-07 11:37:09 +0530

    project added

---
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -0,0 +1,47 @@
+package com.springrest.springrest.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+	@Id
+	private long id;
+	private String title;
+	private String descrption;
+	public Course(long id, String title, String descrption) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.descrption = descrption;
+	}
+	public Course() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescrption() {
+		return descrption;
+	}
+	public void setDescrption(String descrption) {
+		this.descrption = descrption;
+	}
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", descrption=" + descrption + "]";
+	}
+	
+	
+
+}
